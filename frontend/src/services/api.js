d1c8d95a66a3ef25734bbd36790a49a00c827a10
Vikas Verma <vikasverm@gmail.com>
2026-06-06 16:09:32 +0530

    Fixed API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://contact-manager-jn7a.onrender.com/",
+  baseURL: "https://contact-manager-jn7a.onrender.com/api",
 });
 
 export default API;
